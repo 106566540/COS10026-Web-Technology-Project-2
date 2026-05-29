13242c8b85175266b458c63e61bc19904b0b188c
106191722 <106191722@student.swin.edu.au>
2026-05-29 11:23:41 +1000

    removed html validation

--- apply.php
+++ apply.php
@@ -47,19 +47,19 @@
 
                 <!-- Job Reference Number: must be exactly 5 alphanumeric characters -->
                 <label for="jobRef">Job Reference Number:</label>
-                <input type="text" id="jobRef" name="jobRef" required>
+                <input type="text" id="jobRef" name="jobRef" >
 
                 <!-- First Name: max 20 characters, letters only -->
                 <label for="fname">First Name:</label>
-                <input type="text" id="fname" name="fname" required>
+                <input type="text" id="fname" name="fname" >
 
                 <!-- Last Name: max 20 characters, letters only -->
                 <label for="lname">Last Name:</label>
-                <input type="text" id="lname" name="lname"  required>
+                <input type="text" id="lname" name="lname"  >
 
                 <!-- Date of Birth: must match dd/mm/yyyy format -->
                 <label for="dob">Date of Birth:</label>
-                <input type="text" id="dob" name="dob" placeholder="dd/mm/yyyy"  required>
+                <input type="text" id="dob" name="dob" placeholder="dd/mm/yyyy"  >
 
             </div>
 
@@ -78,15 +78,15 @@
 
                 <!-- Street address input -->
                 <label for="street">Street Address:</label>
-                <input type="text" id="street" name="street" required>
+                <input type="text" id="street" name="street">
 
                 <!-- Suburb or town input -->
                 <label for="suburb">Suburb/Town:</label>
-                <input type="text" id="suburb" name="suburb"  required>
+                <input type="text" id="suburb" name="suburb" >
 
                 <!-- State selection dropdown -->
                 <label for="state">State:</label>
-                <select id="state" name="state" required>
+                <select id="state" name="state" >
 
          <!-- Default empty option forces user selection -->
          <option value="">Select</option>
@@ -104,15 +104,15 @@
 
                 <!-- Postcode: must be exactly 4 digits -->
                 <label for="postcode">Postcode:</label>
-                <input type="text" id="postcode" name="postcode"  required>
+                <input type="text" id="postcode" name="postcode"  >
 
                 <!-- Email input with built-in validation -->
                 <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
+                <input type="email" id="email" name="email" >
 
                 <!-- Phone number: must be between 8 and 12 digits -->
                 <label for="phone">Phone Number:</label>
-                <input type="text" id="phone" name="phone"  required>
+                <input type="text" id="phone" name="phone" >
 
             </div>
 
