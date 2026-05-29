58e44350aa4e39cba195f235e0799965ea462c21
106191722 <106191722@student.swin.edu.au>
2026-05-29 11:50:04 +1000

    fixed some typos

--- process_eoi.php
+++ process_eoi.php
@@ -3,7 +3,7 @@ header(loaction : "apply.php");
 exit();
 }
 require_once("settings.php");
-$conn= mysqli_connect($host, $user, $pwd, job_portal);
+$conn= mysqli_connect($host, $user, $pwd, $sql_db);
 if (!$conn) {
 die("Database connection failed:". mysqli_connect_error());
 }
