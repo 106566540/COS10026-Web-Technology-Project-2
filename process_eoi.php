ae2bc56b2fada267aed2455dad6b56a7d5646faf
106191722 <106191722@student.swin.edu.au>
2026-05-29 11:46:09 +1000

    add sanitisation function and post method

--- process_eoi.php
+++ process_eoi.php
@@ -7,3 +7,26 @@ $conn= mysqli_connect($host, $user, $pwd, $sql_db);
 if (!$conn) {
 die("Database connection failed:". mysqli_connect_error());
 }
+function sanitise_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+
+$jobRef = sanitise_input($_POST["jobRef"]);
+$fname = sanitise_input($_POST["fname"]);
+$lname = sanitise_input($_POST["lname"]);
+$dob = sanitise_input($_POST["dob"]);
+$gender = sanitise_input($_POST["gender"]);
+
+$street = sanitise_input($_POST["street"]);
+$suburb = sanitise_input($_POST["suburb"]);
+$state = sanitise_input($_POST["state"]);
+$postcode = sanitise_input($_POST["postcode"]);
+
+$email = sanitise_input($_POST["email"]);
+$phone = sanitise_input($_POST["phone"]);
+
+$other_skills = sanitise_input($_POST["other"]);
