c44955fc49f4c4449c35de89d46ec13c0c575ad7
106191722 <106191722@student.swin.edu.au>
2026-05-29 11:13:48 +1000

    removed html validation of most of the fields

--- apply.php
+++ apply.php
@@ -47,19 +47,19 @@
 
                 <!-- Job Reference Number: must be exactly 5 alphanumeric characters -->
                 <label for="jobRef">Job Reference Number:</label>
-                <input type="text" id="jobRef" name="jobRef" pattern="[A-Za-z0-9]{5}" required>
+                <input type="text" id="jobRef" name="jobRef" required>
 
                 <!-- First Name: max 20 characters, letters only -->
                 <label for="fname">First Name:</label>
-                <input type="text" id="fname" name="fname" maxlength="20" pattern="[A-Za-z]+" required>
+                <input type="text" id="fname" name="fname" required>
 
                 <!-- Last Name: max 20 characters, letters only -->
                 <label for="lname">Last Name:</label>
-                <input type="text" id="lname" name="lname" maxlength="20" pattern="[A-Za-z]+" required>
+                <input type="text" id="lname" name="lname"  required>
 
                 <!-- Date of Birth: must match dd/mm/yyyy format -->
                 <label for="dob">Date of Birth:</label>
-                <input type="text" id="dob" name="dob" placeholder="dd/mm/yyyy" pattern="\d{2}/\d{2}/\d{4}" required>
+                <input type="text" id="dob" name="dob" placeholder="dd/mm/yyyy"  required>
 
             </div>
 
@@ -78,11 +78,11 @@
 
                 <!-- Street address input -->
                 <label for="street">Street Address:</label>
-                <input type="text" id="street" name="street" maxlength="40" required>
+                <input type="text" id="street" name="street" required>
 
                 <!-- Suburb or town input -->
                 <label for="suburb">Suburb/Town:</label>
-                <input type="text" id="suburb" name="suburb" maxlength="40" required>
+                <input type="text" id="suburb" name="suburb"  required>
 
                 <!-- State selection dropdown -->
                 <label for="state">State:</label>
@@ -104,7 +104,7 @@
 
                 <!-- Postcode: must be exactly 4 digits -->
                 <label for="postcode">Postcode:</label>
-                <input type="text" id="postcode" name="postcode" pattern="\d{4}" required>
+                <input type="text" id="postcode" name="postcode"  required>
 
                 <!-- Email input with built-in validation -->
                 <label for="email">Email:</label>
@@ -112,7 +112,7 @@
 
                 <!-- Phone number: must be between 8 and 12 digits -->
                 <label for="phone">Phone Number:</label>
-                <input type="text" id="phone" name="phone" pattern="\d{8,12}" required>
+                <input type="text" id="phone" name="phone"  required>
 
             </div>
 
