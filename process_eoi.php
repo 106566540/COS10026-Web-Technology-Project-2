458b4eafbb50df878d51867413bb4a583290996d
106191722 <106191722@student.swin.edu.au>
2026-05-29 11:30:34 +1000

    database connected

--- process_eoi.php
+++ process_eoi.php
@@ -2,4 +2,8 @@
 header(loaction : "apply.php");
 exit();
 }
-require_once("settings.php")
+require_once("settings.php");
+$conn= mysqli_connect($host, $user, $pwd, $sql_db);
+if (!$conn) {
+die("Database connection failed:". mysqli_connect_error());
+}
