abb62837fe989a7827dccf13ef4505e889e16311
HM-Devo <hjmac2008@icloud.com>
2026-05-31 16:10:28 +1000

    changed two names in process_eoi to prevent errors (MySQL code, first_name and last_name)

--- process_eoi.php
+++ process_eoi.php
@@ -107,7 +107,7 @@ exit();
 }
 
 // Insert into DB and confirm auto generated EOI number
-$query = "INSERT INTO eoi (jobRef, fname, lname, dob, gender, street, suburb, state, postcode, email, phone, skills, other_skills)
+$query = "INSERT INTO eoi (jobRef, first_name, last_name, dob, gender, street, suburb, state, postcode, email, phone, skills, other_skills)
           VALUES ('$jobRef', '$fname', '$lname', '$dob', '$gender', '$street', '$suburb', '$state', '$postcode', '$email', '$phone', '$skills', '$other_skills')";
 
 if (mysqli_query($conn, $query)) {
