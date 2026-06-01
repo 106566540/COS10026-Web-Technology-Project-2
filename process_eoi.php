3bcc828acb70cd62acc70fd493d3a8e073bbd6a1
HM-Devo <hjmac2008@icloud.com>
2026-06-01 21:11:51 +1000

    undoing previous commit (was a table issue, not a code issue)

--- process_eoi.php
+++ process_eoi.php
@@ -124,14 +124,10 @@ if (!empty($errors)) {
 
 // Insert into DB and confirm auto generated EOI number
 $query = "INSERT INTO eoi 
-(jobRef, first_name, last_name, dob, gender, street, suburb, state, postcode, email, phone, skills, other_skills)
+(job_reference, first_name, last_name, dob, gender, street_address, suburb, state, postcode, email, phone, skills, other_skills)
 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 $stmt = mysqli_prepare($conn, $query);
-
-if (!$stmt) {
-    die("Prepare failed: " . mysqli_error($conn));
-}
 
 mysqli_stmt_bind_param(
         $stmt,
